7ee3728f5a6633336325396d0b68f41854b6ead0
Rawezh <rawezhmarif@gmail.com>
2021-05-30 10:19:49 +0300

    setup basic box (failed)

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-(function(){
-    let gameBoard = [];
-    let playerX =  player('X')
-    let playerO =  player('O')
-}())
-
 const player = (name) => {
     let turn = false;
     return {name, turn};
 };
+
+(function(){
+    let gameBoard = [];
+    let playerX =  player('X')
+    let playerO =  player('O')
+}());
+
+
 
 function showBoard(){
     let boardCont = document.createElement('div')
     for (i=0;i<9;i++){
         let cell = document.createElement('div')
         cell.classList.add('cell')
+        cell.innerHTML = '-'
         boardCont.appendChild(cell)
     }
+    boardCont.classList.add('container')
     document.body.appendChild(boardCont)
-
-}
+};
+showBoard();
