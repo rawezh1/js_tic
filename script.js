ae3ebbddfe5212cba8778184c906d8ddecc7d57f
Rawezh <rawezhmarif@gmail.com>
2021-05-30 08:16:30 +0300

    add shoiw board function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 (function(){
     let gameBoard = [];
-    let playerX = new player('X')
-    let playerO = new player('O')
+    let playerX =  player('X')
+    let playerO =  player('O')
 }())
+
+const player = (name) => {
+    let turn = false;
+    return {name, turn};
+};
+
+function showBoard(){
+    let boardCont = document.createElement('div')
+    for (i=0;i<9;i++){
+        let cell = document.createElement('div')
+        cell.classList.add('cell')
+        boardCont.appendChild(cell)
+    }
+    document.body.appendChild(boardCont)
+
+}
