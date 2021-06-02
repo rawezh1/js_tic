a3ced6503fe593cc06436d2a353558b9dd620066
Rawezh <rawezhmarif@gmail.com>
2021-06-02 11:09:26 +0300

    change div to buttons, add event to button and style buttons

--- script.js
+++ script.js
@@ -3,23 +3,31 @@ const player = (name) => {
     return {name, turn};
 };
 
-(function(){
-    let gameBoard = [];
-    let playerX =  player('X')
-    let playerO =  player('O')
-}());
+const game = (function(){
+    let gameBoard = new Array(9).fill('-');
+    let playerX =  player('X');
+    let playerO =  player('O');
+    startTurn(gameBoard,playerX);
+    return {gameBoard}
+})();
 
 
 
-function showBoard(){
-    let boardCont = document.createElement('div')
+function startTurn(board,player){
+    document.body.innerHTML ='';
+    let boardCont = document.createElement('div');
     for (i=0;i<9;i++){
-        let cell = document.createElement('div')
-        cell.classList.add('cell')
-        cell.innerHTML = '-'
-        boardCont.appendChild(cell)
+        let cell = document.createElement('button');
+        cell.classList.add('cell');
+        cell.innerHTML = board[i];
+        cell.id = `${i}`;
+        cell.addEventListener('click',function (element){
+            if (board[i] != '-') {return}
+            element.innerHTML = player.name
+            gameBoard[i] = player.name 
+        });
+        boardCont.appendChild(cell);
     }
-    boardCont.classList.add('container')
-    document.body.appendChild(boardCont)
+    boardCont.classList.add('container');
+    document.body.appendChild(boardCont);
 };
-showBoard();
