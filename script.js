41332eaa02b216cd28160cba4edede9a9daab30c
Rawezh <rawezhmarif@gmail.com>
2021-06-03 12:50:59 +0300

    basic version without check for victory

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
 const player = (name) => {
     let turn = false;
-    const changeTurn = () => {turn = !turn;}
-    return {name, turn, changeTurn};
+    return {name, turn};
 };
 
 const game = (function(){
-    let gameBoard = new Array(9).fill('-');
+    let gameBoard = new Array(9);
     let playerX =  player('X');
     let playerO =  player('O');
-    playerX.changeTurn();
-    startTurn(gameBoard,playerX);
-    return {gameBoard}
+    createBoard()
+    //startTurn(gameBoard,playerX);
+    return {gameBoard, playerO, playerX}
 })();
 
+function createBoard(){
+    let boardCont = document.createElement('div');
+    for (i=0;i<9;i++){
+        let cell = document.createElement('button');
+        cell.classList.add('cell');
+        cell.id = `${i}`;
+        cell.addEventListener('click',function (event){
+            changeSymbol(event.target)
+        });
+        boardCont.appendChild(cell);
+    }
+    boardCont.classList.add('container');
+    document.body.appendChild(boardCont);
+};
 
-
+function changeSymbol(element){
+    if (element.innerHtml = '') {return};
+    console.log(game.playerO.turn)
+    if (game.playerO.turn) {
+        element.innerHTML = 'O';
+        game.gameBoard[element.id] = 'O';
+        element.style.backgroundColor = 'green';
+        game.playerO.turn = !game.playerO.turn;
+        console.log(game.playerO.turn)
+    }
+    else {
+        element.innerHTML = 'X'
+        game.gameBoard[element.id] = 'X';
+        element.style.backgroundColor = 'red';
+        game.playerO.turn = !game.playerO.turn;
+        }
+    
+}
 function startTurn(board,player){
     document.body.innerHTML ='';
     let boardCont = document.createElement('div');
     for (i=0;i<9;i++){
         let cell = document.createElement('button');
         cell.classList.add('cell');
-        cell.innerHTML = board[i];
+        if (board[i] != null) {cell.innerHTML = board[i];}
         cell.id = `${i}`;
-        cell.addEventListener('click',function (element){
-            if (board[i] != '-') {return}
-            element.innerHTML = player.name
-            gameBoard[i] = player.name
+        cell.addEventListener('click',function (){
+            if (!board[i]) {
+                cell.innerHTML = player.name;
+                game.gameBoard[i] = player.name;
+                cell.style.backgroundColor = 'red'
+            }
         });
         boardCont.appendChild(cell);
     }
